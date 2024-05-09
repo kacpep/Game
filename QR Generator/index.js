d09a95d524b936d6d2a837647445efd0c2ff3853
DrFrezze71 <104517232+DrFrezze71@users.noreply.github.com>
2024-05-09 11:06:31 +0200

    feat: Start Project

---
+++ QR Generator/index.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('qr-form');
+const qrCodeContainer = document.getElementById('qr-code');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const gameName = document.getElementById('gameName').value.trim();
+    const NumberOfQuestions = document.getElementById('NumberOfQuestions').value.trim();
+    const FirstLocationName = document.getElementById('FirstLocationName').value.trim();
+    const Coordinates = document.getElementById('Coordinates').value.trim().split(",");
+
+    function generateUUID() {
+        return 'xxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0,
+                v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+
+    const gameId = generateUUID()
+    
+    
+
+    // if (question === '' || location === '') {
+    //     alert('Proszę wypełnić wszystkie pola!');
+    //     return;
+    // }
+
+    const data = {
+        gameId: gameId,
+        gameName: gameName,
+        NumberOfQuestions: NumberOfQuestions,
+        FirstLocationName:FirstLocationName,
+        Coordinates:Coordinates
+    };
+
+    generateQRCode(data);
+});
+
+function generateQRCode(data) {
+    const qrCodeText = JSON.stringify(data);
+    qrCodeContainer.innerHTML = ''; 
+    new QRCode(qrCodeContainer, qrCodeText);
+}
